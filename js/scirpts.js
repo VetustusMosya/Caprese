dfbe49d0772cf34938086604e7cdee687e4a6915
Moiseev <moiseevsenya2002@gmail.com>
2023-01-24 14:37:40 +0300

    Create slider & add students pizza

--- js/scirpts.js
+++ js/scirpts.js
@@ -5,9 +5,11 @@ window.onload = function () {
     document.querySelector(".main__logo").classList.add("animated__logo");
   } catch (error) { }
 
-  const modalTimerId = document.location.pathname !== '/index.html' ? "9" : setTimeout(openModal, 5000);
+  let modalTimerId = document.location.pathname === '/' ? setTimeout(openModal, 5000) : "time";
 
   document.querySelector('html').classList.remove('hidden');
+
+  // !  Modal
 
   const modalTrigger = document.querySelector("[data-modal]"),
     modal = document.querySelector(".modal"),
@@ -42,4 +44,30 @@ window.onload = function () {
     }
   });
 
+  // ! Slider
+
+  const attArr = document.querySelectorAll("[data-img]"),
+    nextBtn = document.querySelector(".modal__next"),
+    backBtn = document.querySelector(".modal__back");
+  let count = 0;
+
+  attArr[count].classList.add('show');
+
+  nextBtn.addEventListener("click", () => {
+    changeSlide(true)
+  });
+
+  backBtn.addEventListener("click", () => {
+    changeSlide(false)
+  });
+
+  function changeSlide(move) {
+    attArr[count].classList.remove('show');
+    if (move) {
+      count = count < attArr.length - 1 ? count + 1 : 0;
+    } else {
+      count = count > 0 ? count - 1 : attArr.length - 1;
+    }
+    attArr[count].classList.add('show');
+  }
 };
